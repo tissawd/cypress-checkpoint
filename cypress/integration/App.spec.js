81a98c32a74846b970af2f28bd4db8bc2662c65b
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-12 16:01:21 -0500

    basic home page cypress tests passing

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -6,4 +6,12 @@ describe('Home page', () => {
   it("Displays the GMDB header in the navigation bar", () => {
     cy.get('nav h1').should('contain', 'GMDB')
   })
+
+  it("Displays a search bar in the navigation bar", () => {
+    cy.get('nav input').should('exist')
+  })
+  
+  it("Displays movie poster images on the home page", () => {
+    cy.get('img').should('exist')
+  })
 })
