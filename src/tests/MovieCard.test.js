2be50f3fc4a1af0a96c76045b3594c1c96563867
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-12 14:10:32 -0500

    movie card renders image tag with testable src

--- src/tests/MovieCard.test.js
+++ src/tests/MovieCard.test.js
@@ -6,4 +6,10 @@ test('should render a MovieCard',()=>{
     render(<MovieCard/>);
     const movieCardElement = screen.getByRole(/img/i);
     expect(movieCardElement).toBeInTheDocument();
-})
+});
+test('should render a image to display GOTG', ()=>{
+    render(<MovieCard/>);
+    const picElement = screen.getByRole(/img/i);
+    expect(picElement.getAttribute("src")).toEqual("https://m.media-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg");
+
+});
